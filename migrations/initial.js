1d87a547c99d1d6626021e8fdfbbeacd58283f8b
Afonso Pereira <afonso.pereira@adene.pt>
2022-10-21 04:55:12 +0100

    Changed: package.json

--- migrations/initial.js
+++ migrations/initial.js
@@ -1,6 +1,8 @@
-import sequelize from '../db';
-/* import User from '../models/User';
-import Relationship from '../models/Relationship';
-import Photo from '../models/Photo'; */
+// import sequelize from '../db.js';
+import User from '../models/User.js';
+import Relationship from '../models/Relationship.js';
+import Photo from '../models/Photo.js';
 
-await sequelize.sync({ alter: true });
+await User.sync({ force: true });
+await Relationship.sync({ force: true });
+await Photo.sync({ force: true });
